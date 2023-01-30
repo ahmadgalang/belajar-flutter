481aabc5f8a3e95b6668da96c5e0bb17f4b2350f
ahmadgalang <258galang@gmail.com>
2023-01-30 15:43:36 +0700

    Text widget and Style

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: const Text('Latihan Flutter')),
-      body: const Center(child: Text('latihan galang flutter sekarang')),
-    ));
+            appBar: AppBar(title: const Text('Latihan Flutter')),
+            body: Center(
+                child: Container(
+                    color: Colors.lightBlue,
+                    width: 150,
+                    height: 150,
+                    child: const Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          'latihan galang flutter sekarang',
+                          textAlign: TextAlign.center,
+                          maxLines: 2,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700),
+                        ))))));
   }
 }
