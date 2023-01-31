8b9994ccc9159a7e7b7873619b61337da3903969
ahmadgalang <258galang@gmail.com>
2023-01-31 16:08:31 +0700

    row and column

--- lib/main.dart
+++ lib/main.dart
@@ -9,22 +9,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(title: const Text('Latihan Flutter')),
-            body: Center(
-                child: Container(
-                    color: Colors.lightBlue,
-                    width: 150,
-                    height: 150,
-                    child: const Align(
-                        alignment: Alignment.center,
-                        child: Text(
-                          'latihan galang flutter sekarang',
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700),
-                        ))))));
+      appBar: AppBar(title: const Text('Latihan row dan Column')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            children: const <Widget>[
+              Text('test data 1 '),
+              Text('test data 2 '),
+              Text('test data 3 ')
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
